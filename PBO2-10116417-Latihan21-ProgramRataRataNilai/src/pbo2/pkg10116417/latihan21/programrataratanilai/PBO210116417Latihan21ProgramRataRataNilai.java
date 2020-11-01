b43d50dbbc1405334a866de01bc20a1e1248b96c
gilangpanjiwp2197 <73225699+gilangpanjiwp2197@users.noreply.github.com>
2020-11-01 11:33:42 +0700

    Add files via upload

---
+++ PBO2-10116417-Latihan21-ProgramRataRataNilai/src/pbo2/pkg10116417/latihan21/programrataratanilai/PBO210116417Latihan21ProgramRataRataNilai.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10116417.latihan21.programrataratanilai;
+import java.util.Scanner;
+/**
+ *
+ * @author Akmal
+ * `NAMA        : GILANG PANJI WANENG PATI
+ *  KELAS       : PBO2
+ *  NIM         : 10116417
+ *  DESKRIPSI   : BERISI TENTANG PROGRAM Rata - Rata Nilai
+ */
+public class PBO210116417Latihan21ProgramRataRataNilai {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+         Scanner input = new Scanner(System.in);
+        int nBanyak, jml, nilai[];
+        float rata, jumlah = 0;
+
+        System.out.print("Masukan Jumlah Mahasiswa : ");
+        nBanyak = input.nextInt();
+
+        nilai = new int[nBanyak];
+
+        for (int i = 0; i <= nBanyak - 1; i++) {
+            System.out.print("Nilai Mahasiswa Ke-" + (i + 1) + " : ");
+            nilai[i] = input.nextInt();
+        }
+        for (int j = 0; j <= nBanyak - 1; j++) {
+            jumlah = jumlah + nilai[j];
+        }
+
+        rata = jumlah / nBanyak;
+        System.out.println();
+        System.out.println("Maka, rata rata nilainya adalah " + rata);
+        System.out.println("Devoloped by  Gilang Panji Waneng Pati");
+    }
+    
+}
